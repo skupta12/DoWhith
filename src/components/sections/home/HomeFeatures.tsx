1fd8d147da3d4f08cbac460003b3ba6a0245080a
skupta12 <tosya2001@gmail.com>
2024-06-10 22:57:56 +0300

    home features tsx

---
+++ src/components/sections/home/HomeFeatures.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const HomeFeatures = () => {
+  return (
+    <>
+      <section>
+
+      </section>
+      <section>
+
+      </section>
+      <section>
+        
+      </section>
+    </>
+  );
+};
+
+export default HomeFeatures;
--- src/components/sections/home/PhysicalCard.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const PhysicalCard = () => {
-  return (
-    <div>PhysicalCard</div>
-  )
-}
-
-export default PhysicalCard
