60e76b281cb1f2d3e4023457f91669ec3071d4f2
skupta12 <tosya2001@gmail.com>
2024-07-13 13:15:52 +0300

    Connect.tsx

---
+++ src/components/sections/home/Connect.tsx
@@ -0,0 +1,16 @@
+import MaxWidthWrapper from '@/components/MaxWidthWrapper'
+import React from 'react'
+
+const Connect = () => {
+  return (
+    <section>
+        <MaxWidthWrapper>
+            <div>
+                
+            </div>
+        </MaxWidthWrapper>
+    </section>
+  )
+}
+
+export default Connect
