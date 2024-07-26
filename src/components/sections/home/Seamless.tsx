e5b4013cc33437c6efc8f41fd00b92ff512a264c
skupta12 <tosya2001@gmail.com>
2024-07-26 13:10:28 +0300

    small changes

--- src/components/sections/home/Seamless.tsx
+++ src/components/sections/home/Seamless.tsx
@@ -18,9 +18,9 @@ const Seamless = () => {
               </h2>
             </div>
           </div>
-          <div className="grid grid-cols-12 gap-x-7 lg:gap-y-7 gap-y-10">
-            <div className="lg:col-span-7 col-span-12 overflow-hidden rounded-xl bg-primary-400 p-[2px]">
-              <div className="flex flex-col gap-y-4 rounded-xl p-5 bg-primary-100 ">
+          <div className="grid grid-cols-12 lg:gap-x-7 gap-x-5 lg:gap-y-7 gap-y-10">
+            <div className="lg:col-span-7 col-span-12 overflow-hidden rounded-xl border border-primary-200 p-[2px] h-full">
+              <div className="flex flex-col gap-y-4 rounded-xl p-5 bg-primary-100">
                 <h3 className={`${styles.heading3V} text-white opacity-80`}>
                   Connect 10+ Integrations
                 </h3>
@@ -29,14 +29,14 @@ const Seamless = () => {
                   workflow and productivity.
                 </p>
                 <Image
-                  className="relative -bottom-16 lg:h-[330px] object-contain"
+                  className="relative left-1/2 transform -translate-x-[50%] -bottom-14"
                   src={SeamlessImageS}
                   alt="seamless-image 1"
                 />
               </div>
             </div>
-            <div className="lg:col-span-5 col-span-12 overflow-hidden rounded-xl bg-primary-400 p-[2px]">
-              <div className="flex flex-col gap-y-4 p-5 rounded-xl bg-primary-100">
+            <div className="lg:col-span-5 col-span-12 overflow-hidden rounded-xl border border-primary-200 p-[2px]">
+              <div className="flex flex-col gap-y-4 p-5 rounded-xl bg-primary-100 border border-white m-3">
                 <h3 className={`${styles.heading3V} text-white opacity-80`}>
                   Connect 10+ Integrations
                 </h3>
@@ -45,7 +45,7 @@ const Seamless = () => {
                   workflow and productivity.
                 </p>
                 <Image
-                  className="relative -bottom-16 lg:h-[330px] object-contain"
+                  className="relative overflow-hidden left-1/2 transform -translate-x-[50%] -bottom-20"
                   src={SeamlessImageF}
                   alt="seamless image 2"
                 />
