e457c4a6d963309e93efa4cfd062c98eb27bfd4e
skupta12 <tosya2001@gmail.com>
2024-08-04 12:14:20 +0300

    reusable component v1.2

--- src/components/DynamicFramerPos.tsx
+++ src/components/DynamicFramerPos.tsx
@@ -1,10 +1,68 @@
-import { ReactNode } from "react";
+import { useScroll, useTransform } from "framer-motion";
+import { ReactNode, useRef } from "react";
 
+const DynamicFramerPos = ({
+  children,
+  initialTransform,
+  finalTransform,
+  opacity,
+}: {
+  initialTransform: string;
+  finalTransform: string;
+  children: ReactNode;
+}) => {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["1.7 1", "10 1"],
+  });
 
-const DynamicFramerPos = ({ x, y, children }: { x: string; y: string; children: ReactNode }) => {
+  const transformStyles = {
+    translateX: useTransform(
+      scrollYProgress,
+      [0, 1],
+      initialTransform.translateX
+    ),
+    translateY: useTransform(
+      scrollYProgress,
+      [0, 1],
+      initialTransform.translateY
+    ),
+  };
+
+  if (finalTransform) {
+    transformStyles.translateX = useTransform(
+      scrollYProgress,
+      [0, 1],
+      finalTransform.translateX
+    );
+    transformStyles.translateY = useTransform(
+      scrollYProgress,
+      [0, 1],
+      finalTransform.translateY
+    );
+  }
+
+  const opacityStyle = opacity ? { opacity: scrollYProgress } : {};
+
   return (
-    <div>{children}</div>
-  )
-}
-
-export default DynamicFramerPos
+    <motion.div
+      className="transition ease-linear duration-75"
+      initial={{ translateX: 0, translateY: 0, opacity: 0 }}
+      ref={ref}
+      style={{ ...transformStyles, ...opacityStyle }}
+    >
+      <div className="bg-white p-2 rounded-lg">
+        <Image
+          width={33}
+          height={32}
+          className="text-center mx-auto mb-1"
+          src={icon}
+          alt={`${label} icon`}
+        />
+        <span className="font-semibold">{label}</span>
+      </div>
+    </motion.div>
+  );
+};
+export default DynamicFramerPos;
